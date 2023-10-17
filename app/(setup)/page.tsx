22d0d296ca00568b371726853df06b5e92547583
mikiyas <mikiyasget1234@gmail.com>
2023-10-17 16:13:37 +0100

    intial commit

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,12 +1,30 @@
 import { ModeToggle } from "@/components/mode-toggle";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
 import { UserButton } from "@clerk/nextjs";
+import { InitialModal } from "@/components/modals/initial-modal";
 
-export default function Home() {
+export default async function Home() {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
   return (
     <div>
-      <div className="text-3xl font-bold text-indigo-500">discord clone</div>
-      <UserButton afterSignOutUrl="/" />
-      <ModeToggle />
+      <InitialModal />
     </div>
   );
 }
