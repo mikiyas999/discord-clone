0a2c300052ffde6b551a864bcb01c38cb1954c96
mikiyas <mikiyasget1234@gmail.com>
2023-10-18 15:41:01 +0100

    update modal store

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -14,10 +14,19 @@ export type ModalType =
   | "messageFile"
   | "deleteMessage";
 
+interface ModalData {
+  server?: Server;
+  channel?: Channel;
+  channelType?: ChannelType;
+  apiUrl?: string;
+  query?: Record<string, any>;
+}
+
 interface ModalStore {
   type: ModalType | null;
+  data: ModalData;
   isOpen: boolean;
-  onOpen: (type: ModalType) => void;
+  onOpen: (type: ModalType, data?: ModalData) => void;
   onClose: () => void;
 }
 
@@ -25,6 +34,6 @@ export const useModal = create<ModalStore>((set) => ({
   type: null,
   data: {},
   isOpen: false,
-  onOpen: (type) => set({ isOpen: true, type }),
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
   onClose: () => set({ type: null, isOpen: false }),
 }));
