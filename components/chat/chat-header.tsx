35125a323d397ba75e02d43f84b9e619b2420ea7
mikiyas <mikiyasget1234@gmail.com>
2023-11-02 15:08:02 +0000

    commit

---
+++ components/chat/chat-header.tsx
@@ -0,0 +1,31 @@
+import { Hash } from "lucide-react";
+import MobileToggle from "../mobile-toggle";
+import { UserAvatar } from "../user-avatar";
+import SocketIndicator from "../socket-indicator";
+
+interface ChatHeaderProps {
+  imageUrl?: string;
+  name: string;
+  serverId: string;
+  type: "conversation" | "channel";
+}
+
+const ChatHeader = ({ imageUrl, name, serverId, type }: ChatHeaderProps) => {
+  return (
+    <div className="text-md font-semibold px-3 flex items-center h-12 border-b-2 border-neutral-200 dark:border-neutral-800 ">
+      <MobileToggle serverId={serverId} />
+      {type === "channel" && (
+        <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className="w-8 h-8 md:w-8 md:h-8 mr-2" />
+      )}
+      <p className="font-semibold text-md text-black dark:text-white">{name}</p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator />
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;
