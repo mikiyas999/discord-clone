b007de45e743a1f2703bd91387788873e2aec6ec
mikiyas <mikiyasget1234@gmail.com>
2023-11-02 15:08:34 +0000

    add mobile toggle for small screen

---
+++ components/mobile-toggle.tsx
@@ -0,0 +1,29 @@
+import { Menu } from "lucide-react";
+import { Button } from "./ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import NavigationSidebar from "./navigation/navigation-sidebar";
+import { ServerSidebar } from "./server/server-sidebar";
+
+interface MobileToggleProps {
+  serverId: string;
+}
+
+const MobileToggle = ({ serverId }: MobileToggleProps) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="flex gap-o p-0">
+        <div className="w-[72px]">
+          <NavigationSidebar />
+        </div>
+        <ServerSidebar serverId={serverId} />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileToggle;
