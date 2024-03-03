d4bc3b589c8d9e0893ed369a5c9d783a4157f918
Mikiyas Getachhew <125805964+mikiyas999@users.noreply.github.com>
2024-03-03 08:31:17 +0300

    Update delete-message-modal.tsx

--- components/modals/delete-message-modal.tsx
+++ components/modals/delete-message-modal.tsx
@@ -48,7 +48,7 @@ export const DeleteMessageModal = () => {
           <DialogTitle className="text-2xl text-center font-bold">
             Delete Message
           </DialogTitle>
-          <DialogDescription className="text-center text-zinc-500">
+          <DialogDescription className="text-center text-zinc-400">
             Are you sure you want to do this? <br />
             This message will be permanently deleted.
           </DialogDescription>
